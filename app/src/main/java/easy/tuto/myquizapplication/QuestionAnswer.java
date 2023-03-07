d484aeaf53001df35a1e1f9d1dfbb00cd947b2db
shruti.sambarwal <shruti.sambarwal2002@gmail.com>
2023-03-07 13:29:49 +0530

    Initial commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Who invented Java Programming?",
+            "Which company owns the Apple?",
+            "Which one of the following is not a Java feature?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Guido van Russum","James Gosling","Dennis Ritchie","Bjarne Stroustrup"},
+            {"Google","Apple","Nokia","Samsung"},
+            {"Object-Oriented","Use of pointers","Poratble","Dynamic and Extensible"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "James Gosling",
+            "Apple",
+            "Use of pointers"
+    };
+
+}
